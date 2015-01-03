9322436c9b8ab9f501c50c719a08df174334abe3
XadillaX <admin@xcoder.in>
2015-01-03 10:04:49 +0800

    with love

--- .vim/templates/skeleton.js
+++ .vim/templates/skeleton.js
@@ -1,5 +1,5 @@
 /**
- * XadillaX created at [:VIM_EVAL:]strftime("%Y-%m-%d %H:%M:%S")[:END_EVAL:]
+ * XadillaX created at [:VIM_EVAL:]strftime("%Y-%m-%d %H:%M:%S")[:END_EVAL:] With ♥
  *
  * Copyright (c) [:VIM_EVAL:]strftime("%Y")[:END_EVAL:] Huaban.com, all rights
  * reserved
