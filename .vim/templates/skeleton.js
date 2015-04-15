b25a26d9b81f51da23ea83ae01ebcb800c1d4aac
Flandre Scarlet <i@2333.moe>
2015-04-15 21:32:52 +0800

    Update skeleton.js

--- .vim/templates/skeleton.js
+++ .vim/templates/skeleton.js
@@ -2,5 +2,5 @@
  * XadillaX created at [:VIM_EVAL:]strftime("%Y-%m-%d %H:%M:%S")[:END_EVAL:] With ♥
  *
  * Copyright (c) [:VIM_EVAL:]strftime("%Y")[:END_EVAL:] Huaban.com, all rights
- * reserved
+ * reserved.
  */
